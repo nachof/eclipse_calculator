b493aa16e10eb8fcde183c3810cc322cae41d3b2
Nacho Facello <nacho@nucleartesuji.com>
2014-11-16 22:26:20 -0200

    Add presets for all default ships

--- app/src/main/java/com/nucleartesuji/eclipsecalculator/calculator/ShipPreset.java
+++ app/src/main/java/com/nucleartesuji/eclipsecalculator/calculator/ShipPreset.java
@@ -16,9 +16,44 @@ public class ShipPreset {
         this.label    = label;
     }
 
+    public static ShipPreset defaultInterceptor() {
+        ShipSpec spec = ShipSpec.builder().setInitiative(3).setIonCannons(1).build();
+        return new ShipPreset(R.string.preset_name_default_interceptor, spec);
+    }
+
     public static ShipPreset defaultCruiser() {
         ShipSpec spec =  ShipSpec.builder().setInitiative(2).setIonCannons(1).setHull(1).setComputer(1).build();
         return new ShipPreset(R.string.preset_name_default_cruiser, spec);
+    }
+
+    public static ShipPreset defaultDreadnought() {
+        ShipSpec spec = ShipSpec.builder().setInitiative(1).setIonCannons(2).setHull(2).setComputer(1).build();
+        return new ShipPreset(R.string.preset_name_default_dreadnought, spec);
+    }
+
+    public static ShipPreset orionInterceptor() {
+        ShipSpec spec = ShipSpec.builder().setInitiative(4).setIonCannons(1).setShield(1).build();
+        return new ShipPreset(R.string.preset_name_orion_interceptor, spec);
+    }
+
+    public static ShipPreset orionCruiser() {
+        ShipSpec spec =  ShipSpec.builder().setInitiative(3).setIonCannons(1).setHull(1).setComputer(1).setShield(1).build();
+        return new ShipPreset(R.string.preset_name_orion_cruiser, spec);
+    }
+
+    public static ShipPreset orionDreadnought() {
+        ShipSpec spec = ShipSpec.builder().setInitiative(2).setIonCannons(2).setHull(2).setComputer(1).setShield(1).build();
+        return new ShipPreset(R.string.preset_name_orion_dreadnought, spec);
+    }
+
+    public static ShipPreset plantaInterceptor() {
+        ShipSpec spec = ShipSpec.builder().setInitiative(1).setIonCannons(1).setComputer(1).build();
+        return new ShipPreset(R.string.preset_name_planta_interceptor, spec);
+    }
+
+    public static ShipPreset plantaCruiser() {
+        ShipSpec spec =  ShipSpec.builder().setInitiative(1).setIonCannons(1).setHull(1).setComputer(1).build();
+        return new ShipPreset(R.string.preset_name_planta_cruiser, spec);
     }
 
     public static ShipPreset ancient() {
@@ -28,7 +63,14 @@ public class ShipPreset {
 
     public static List<ShipPreset> collection() {
         List<ShipPreset> result = new ArrayList<ShipPreset>();
+        result.add(defaultInterceptor());
         result.add(defaultCruiser());
+        result.add(defaultDreadnought());
+        result.add(orionInterceptor());
+        result.add(orionCruiser());
+        result.add(orionDreadnought());
+        result.add(plantaInterceptor());
+        result.add(plantaCruiser());
         result.add(ancient());
         return result;
     }
